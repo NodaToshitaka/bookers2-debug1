18469430f90cf5c82cc1f0d423338de02fd8766a
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2021-12-21 10:19:00 +0000

    compleate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
   end
 
   def index
@@ -24,6 +26,16 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def follows
+    @user = User.find(params[:id])
+    @users = @user.following_user.all
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.follower_user.all
   end
 
   private
