0335c14414f35340c3c044a8a4da04bb7c18338b
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2021-12-24 10:39:56 +0000

    star

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery3
+//= require jquery_raty.js
 //= require popper
 //= require bootstrap-sprockets
 //= require rails-ujs
--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,11 @@ class Book < ApplicationRecord
 	validates :title, presence: true
 	validates :body, presence: true, length: {maximum: 200}
 
+	validates :rate, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }, presence: true
+
 	def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
